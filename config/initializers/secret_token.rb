f1f56c5e940c70635312e8e5e86667dd6a222ecd
Lenny Marks <lenny@aps.org>
2012-11-14 11:16:39 -0500

    Start with a freshly generated rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rails39AutoloadIssue::Application.config.secret_token = 'f6bfe645f710c425f5bab8dda5545c544d10e4f853d3f66f76c988940fee911812ba06d078134c0d7e99dbba80653783a0b3e33fa4012b93bba6d91287859e9b'
